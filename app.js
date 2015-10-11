9750cbb4b3d964d89edd17c3e29a4e277c373908
1000ch <shogo.sensui@gmail.com>
2015-10-12 02:02:43 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const escapeMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#x27;'
+};
+
+const unescapeMap = {
+  '&amp;': '&',
+  '&lt;': '<',
+  '&gt;': '>',
+  '&quot;': '"',
+  '&#x27;': "'"
+};
+
+const escapeRegex = new RegExp(`[${Object.keys(escapeMap).join('')}]`, 'g');
+const unescapeRegex = new RegExp(`(${Object.keys(unescapeMap).join('|')})`, 'g');
+
+const escapeHTML = string => string.replace(escapeRegex, match => escapeMap[match]);
+const unescapeHTML = string => string.replace(unescapeRegex, match => unescapeMap[match]);
+
+window.onload = e => {
+
+  'use strict';
+
+  const escapeInput  = document.querySelector('#escape-input');
+  const escapeOutput = document.querySelector('#escape-output');
+  const unescapeInput  = document.querySelector('#unescape-input');
+  const unescapeOutput = document.querySelector('#unescape-output');
+
+  escapeInput.addEventListener('input', e => escapeOutput.value = escapeHTML(escapeInput.value));
+  unescapeInput.addEventListener('input', e => unescapeOutput.value = unescapeHTML(unescapeInput.value));
+};
