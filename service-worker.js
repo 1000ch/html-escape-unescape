de5c3fc3e46402ba8f335010e02fd3f51a4c1963
1000ch <shogo.sensui@gmail.com>
2016-02-03 01:25:47 +0900

    remove old cache

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,29 @@
 const CACHE_KEY = 'v1';
 
 self.addEventListener('install', e => {
-
   e.waitUntil(
     caches.open(CACHE_KEY).then(cache => {
       return cache.addAll([
         'index.html',
         'app.js'
       ]);
-    }).catch(e => console.log(e))
+    }).catch(e => console.error(e))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_KEY)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    }).catch(e => console.error(e))
   );
 });
 
 self.addEventListener('fetch', e => {
-
   e.respondWith(
     caches.open(CACHE_KEY).then(cache => {
       return cache.match(e.request).then(response => {
@@ -22,6 +32,6 @@ self.addEventListener('fetch', e => {
           return response;
         });
       });
-    })
+    }).catch(e => console.error(e))
   );
 });
